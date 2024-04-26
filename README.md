b99821386e9f8bdbcd1ab0b95585dfba7f2a9ecc
apandey16 <apande02@calpoly.edu>
2024-04-26 14:29:14 -0700

    added graph visualization

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # csc480_lab3 -- Uninformed Seach Algorithms
 
 Implmented DFS, BFS, and Uniform search algoritms that generate a path between two random points on a randomly generate graph in `nav_system.py`.
+
+When running the code, adding the -v in the command line argrument will create a visualization of the graph
--- nav_system.py
+++ nav_system.py
@@ -2,6 +2,9 @@ from dfs import dfs
 from bfs import bfs
 from uniform import uniform_cost_search
 from random import randint, random
+import networkx as nx
+import matplotlib.pyplot as plt
+import sys
 
 def generate_nodes(nodes):
     nodes_list =[]
@@ -10,7 +13,7 @@ def generate_nodes(nodes):
     return nodes_list
 
 # Constants for graph generation
-NODES = generate_nodes(15)
+NODES = generate_nodes(10)
 MAX_COST = 10
 START = NODES[randint(0, len(NODES)-1)]
 END = START
@@ -28,12 +31,32 @@ def generate_graph(nodes, max_cost):
         graph[node][neighbor] = cost
         for neighbor in nodes:
             if neighbor != node and neighbor not in graph[node]:
-                if random() < 0.5:
+                if random() < 0.25:
                     cost = randint(1, max_cost)
                     graph[node][neighbor] = cost
     return graph
+
+def create_visualization(graph):
+    G = nx.DiGraph()
+    for node in graph:
+        G.add_node(node)
+    for node in graph:
+        for neighbor, cost in graph[node].items():
+            G.add_edge(node, neighbor, weight=cost)
+
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=500, font_size=10, edge_color='gray', width=1, arrowsize=10)
+
+    edge_labels = nx.get_edge_attributes(G, 'weight')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='red')
+    plt.show()
+    
+    sys.exit()
+    
+
 def main():
     graph = generate_graph(NODES, MAX_COST)
+
     print("Graph: " + str(graph))
     print("Start: " + str(START))
     print("End: " + str(END))
@@ -59,6 +82,9 @@ def main():
         uniform_cost_search_path = "No path found"
 
     print("Uniform Search Path: " + str(uniform_cost_search_path))
+
+    if (len(sys.argv) > 1 and sys.argv[1] == '-v'):
+        create_visualization(graph)
 
 if __name__ == '__main__':
     main()
